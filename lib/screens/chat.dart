565701ebdcc6a974dfb2d606cded92fdcd613aa4
Neel Patel <patelneel1119@gmail.com>
2023-08-27 11:13:52 +0530

    splash screen | logout | profile image picker

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -8,11 +10,21 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Chat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.logout,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          )
+        ],
       ),
       body: const Center(
         child: Text('Logged In!'),
       ),
     );
-
   }
 }
